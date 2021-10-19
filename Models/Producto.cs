ce27926f53db1b5f15ef693e6af7167a6bda7b39
Arnold1709 <arnold_guerrero1@usmp.pe>
2021-10-19 18:32:07 -0500

    campos obligatorios

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,15 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace practica3.Models
 {
    public class Producto
     {
+        [Required]
         public int Id {get; set; }
+        [Required]
         public string NomProducto {get;set; }
+        [Required]
         public string Foto {get;set; }
+        [Required]
         public string  Descripcion {get; set;}
+        [Required]
         public string  Precio {get;set;}
-       
+       [Required]
         public string Celular {get;set;}
+        [Required]
        public string Lugar {get;set;}
+       [Required]
 
        public string Nombre {get;set;}
         public Categoria Categoria {get;set;}
